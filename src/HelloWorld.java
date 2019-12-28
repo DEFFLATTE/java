513936289bb8812698c7da98e85c8747f0f4e4b5
zhou <1020812251@qq.com>
2019-12-28 19:15:15 +0800

    first submit

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Created by zhouhangfeng on 2019/12/28.
+ */
+public class HelloWorld {
+    /*
+    * 注释
+    *
+    * */
+    public static void main(String []args){
+        //单行注释
+        /*单行注释*/
+        System.out.println("Hello World");
+    }
+}
---
+++ src/Index.java
@@ -0,0 +1,9 @@
+/**
+ * Created by zhouhangfeng on 2019/12/28.
+ */
+public class Index {
+    public  static  void main(String []args) {
+        System.out.println("Hello World");
+    }
+}
+
